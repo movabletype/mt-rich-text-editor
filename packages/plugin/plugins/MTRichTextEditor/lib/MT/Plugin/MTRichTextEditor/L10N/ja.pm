c02074e0cf919989cefc2578f124db099999d139
Taku Amano <taku@taaas.jp>
2024-07-02 12:42:42 +0900

    Add L10N placeholders

---
+++ packages/plugin/plugins/MTRichTextEditor/lib/MT/Plugin/MTRichTextEditor/L10N/ja.pm
@@ -0,0 +1,18 @@
+# Movable Type (r) (C) 2006-2019 Six Apart Ltd. All Rights Reserved.
+# This code cannot be redistributed without permission from www.sixapart.com.
+# For more information, consult your Movable Type license.
+#
+# $Id$
+
+package MT::Plugin::MTRichTextEditor::L10N::ja;
+
+use strict;
+use warnings;
+use utf8;
+
+use base 'MT::Plugin::MTRichTextEditor::L10N::en_us';
+use vars qw( %Lexicon );
+
+%Lexicon = ();
+
+1;
