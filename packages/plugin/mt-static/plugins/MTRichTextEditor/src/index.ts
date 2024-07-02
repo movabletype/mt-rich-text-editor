d44b3b9636e0d5a7d6ca669face9cc8a58aa5c65
Taku Amano <taku@taaas.jp>
2024-07-02 12:24:43 +0900

    add types to bootstrap script in plugin

--- packages/plugin/mt-static/plugins/MTRichTextEditor/src/index.ts
+++ packages/plugin/mt-static/plugins/MTRichTextEditor/src/index.ts
@@ -1,14 +1,19 @@
 import "@movabletype/mt-rich-text-editor/dist/style.css";
 import { create } from "@movabletype/mt-rich-text-editor";
 
-MT.Editor.MTRichTextEditor = function (...args) {
+interface MTRichTextEditor {
+  id: string;
+  initEditor: (format: unknown) => void;
+}
+
+MT.Editor.MTRichTextEditor = function (...args: unknown[]) {
   MT.Editor.apply(this, ...args);
 };
 Object.assign(MT.Editor.MTRichTextEditor, MT.Editor);
 Object.assign(MT.Editor.MTRichTextEditor.prototype, MT.Editor.prototype);
 Object.assign(MT.Editor.MTRichTextEditor.prototype, {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  initEditor: function (format) {
+  initEditor: function (this: MTRichTextEditor, _: unknown) {
     create("#" + this.id);
   },
 });
