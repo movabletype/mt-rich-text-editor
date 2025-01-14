43ad7aabe84a7d27a90c7d13c0550acc128171a6
Taku Amano <taku@taaas.jp>
2025-01-14 11:16:44 +0900

    Enable to insert boilerplate via quick action

--- packages/plugin/mt-static/plugins/FormattedTextForMTRichTextEditor/extension.js
+++ packages/plugin/mt-static/plugins/FormattedTextForMTRichTextEditor/extension.js
@@ -1,10 +1,55 @@
 const scriptElm = document.querySelector('script[data-formatted-text-for-mt-rich-text-editor]');
 const boilerplates = JSON.parse(scriptElm.getAttribute('data-formatted-text-for-mt-rich-text-editor'));
+
+boilerplates.forEach((boilerplate) => {
+  customElements.define(`mt-rich-text-editor-quick-action-item-boilerplate-${boilerplate.id}`, class extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({ mode: "open" }).innerHTML = boilerplate.title;
+    }
+
+    onEditorInit(editor, options) {
+      this.editor = editor;
+      this.options = options;
+    }     
+
+    connectedCallback() {
+      this.addEventListener("click", () => {
+        console.log(this.editor, this.options);
+        this.editor.tiptap
+          ?.chain()
+          .focus()
+          .selectParentNode()
+          // .deleteSelection()
+          .insertContent(boilerplate.text)
+          .run();
+      });
+    }
+  });
+});
+
 MTRichTextEditor.on("create", (config) => {
   config.toolbarOptions = {
     ...config.toolbarOptions,
     boilerplate: boilerplates.length === 0 ? false : {
       boilerplates,
     },
+  };
+
+  config.quickAction = [
+    ...config.quickAction,
+    ...boilerplates.map((boilerplate) => `boilerplate-${boilerplate.id}`)
+  ]
+
+  config.quickActionOptions = {
+    ...config.quickActionOptions,
+    ...Object.fromEntries(boilerplates.map((boilerplate) => [
+      `boilerplate-${boilerplate.id}`,
+      {
+        aliases: [
+          boilerplate.text.match(/<(\w+)>/)?.[1]
+        ]
+      }
+    ])),
   };
 });
