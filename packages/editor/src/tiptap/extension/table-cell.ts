595784ce51faf3d49441f56de57e3e86c2e0d0e3
Taku Amano <taku@taaas.jp>
2025-03-19 16:47:36 +0900

    Enable to set style and scope via table API.

--- packages/editor/src/tiptap/extension/table-cell.ts
+++ packages/editor/src/tiptap/extension/table-cell.ts
@@ -4,13 +4,23 @@ import { TableCell as TiptapTableCell } from "@tiptap/extension-table-cell";
 export const TableCell = TiptapTableCell.extend({
   content: "inline*",
 
+  addAttributes() {
+    return {
+      ...this.parent?.(),
+      style: {
+        default: "",
+      },
+    };
+  },
+
   renderHTML({ HTMLAttributes }) {
-    const { colspan, rowspan, ...rest } = HTMLAttributes;
+    const { colspan, rowspan, style, ...attrs } = HTMLAttributes;
     return [
       "td",
-      mergeAttributes(rest, {
+      mergeAttributes(attrs, {
         colspan: colspan === 1 ? undefined : colspan,
         rowspan: rowspan === 1 ? undefined : rowspan,
+        style: style === "" ? undefined : style,
       }),
       0,
     ];
