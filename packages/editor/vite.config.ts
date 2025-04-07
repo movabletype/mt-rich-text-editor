d6b2221132a0ed87cdfffa24817e32b6d83f4647
Taku Amano <taku@taaas.jp>
2025-04-07 17:50:15 +0900

    Set resolve.resolve only when mode is test

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -1,45 +1,51 @@
 import { defineConfig } from "vite";
+import type { ViteConfig } from "vite";
 import dts from "vite-plugin-dts";
 import postcssNesting from "postcss-nesting";
 import postcssInlineSvg from "postcss-inline-svg";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
-const resolveConditions = [
-  "module",
-  "browser",
-  process.env.NODE_ENV === "production" ? "production" : "development",
-];
-
-export default defineConfig(({ mode }) => ({
-  build: {
-    lib: {
-      entry: ["src/index.ts", "src/component.ts", "src/mt-rich-text-editor.ts"],
-      formats: ["es"],
-    },
-    sourcemap: true,
-    minify: true,
-  },
-  plugins: [
-    dts(),
-    svelte({
-      compilerOptions: {
-        customElement: true,
+export default defineConfig(({ mode }) => {
+  const config: ViteConfig = {
+    build: {
+      lib: {
+        entry: ["src/index.ts", "src/component.ts", "src/mt-rich-text-editor.ts"],
+        formats: ["es"],
       },
-    }),
-  ],
-  css: {
-    postcss: {
-      plugins: [postcssNesting, postcssInlineSvg],
+      sourcemap: true,
+      minify: true,
     },
-  },
-  test: {
-    environment: "jsdom",
-    globals: true,
-    watch: false,
-    include: ["src/**/*.test.ts"],
-  },
-  resolve: {
-    // currently returns the same value, but may return a different value in the future when test
-    conditions: mode === "test" ? resolveConditions : resolveConditions,
-  },
-}));
+    plugins: [
+      dts(),
+      svelte({
+        compilerOptions: {
+          customElement: true,
+        },
+      }),
+    ],
+    css: {
+      postcss: {
+        plugins: [postcssNesting, postcssInlineSvg],
+      },
+    },
+    test: {
+      environment: "jsdom",
+      globals: true,
+      watch: false,
+      include: ["src/**/*.test.ts"],
+    },
+  };
+
+  if (mode === "test") {
+    // XXX: svelte requires "module" condition, but vitest drops it, so we need to add it back
+    config.resolve = {
+      conditions: [
+        "module",
+        "browser",
+        process.env.NODE_ENV === "production" ? "production" : "development",
+      ],
+    };
+  }
+
+  return config;
+});
