8480567dbcfad73ca40f7d3a4940013fe1e92d73
Taku Amano <taku@taaas.jp>
2025-04-08 09:36:40 +0900

    Remove resolve conditions to simplify vite.config.ts

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -1,51 +1,34 @@
-import { defineConfig } from "vite";
-import type { ViteConfig } from "vite";
 import dts from "vite-plugin-dts";
 import postcssNesting from "postcss-nesting";
 import postcssInlineSvg from "postcss-inline-svg";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
-export default defineConfig(({ mode }) => {
-  const config: ViteConfig = {
-    build: {
-      lib: {
-        entry: ["src/index.ts", "src/component.ts", "src/mt-rich-text-editor.ts"],
-        formats: ["es"],
+export default {
+  build: {
+    lib: {
+      entry: ["src/index.ts", "src/component.ts", "src/mt-rich-text-editor.ts"],
+      formats: ["es"],
+    },
+    sourcemap: true,
+    minify: true,
+  },
+  plugins: [
+    dts(),
+    svelte({
+      compilerOptions: {
+        customElement: true,
       },
-      sourcemap: true,
-      minify: true,
+    }),
+  ],
+  css: {
+    postcss: {
+      plugins: [postcssNesting, postcssInlineSvg],
     },
-    plugins: [
-      dts(),
-      svelte({
-        compilerOptions: {
-          customElement: true,
-        },
-      }),
-    ],
-    css: {
-      postcss: {
-        plugins: [postcssNesting, postcssInlineSvg],
-      },
-    },
-    test: {
-      environment: "jsdom",
-      globals: true,
-      watch: false,
-      include: ["src/**/*.test.ts"],
-    },
-  };
-
-  if (mode === "test") {
-    // XXX: svelte requires "module" condition, but vitest drops it, so we need to add it back
-    config.resolve = {
-      conditions: [
-        "module",
-        "browser",
-        process.env.NODE_ENV === "production" ? "production" : "development",
-      ],
-    };
-  }
-
-  return config;
-});
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    watch: false,
+    include: ["src/**/*.test.ts"],
+  },
+};
