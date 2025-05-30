4750614645dd4176ab285376ffa0b406c6347783
Taku Amano <taku@taaas.jp>
2025-05-30 13:18:58 +0900

    fix: types in vite.config.ts

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
-import type { ViteConfig } from "vite";
+import type { ViteUserConfig } from "vitest/config";
 import dts from "vite-plugin-dts";
 import postcssNesting from "postcss-nesting";
 import postcssInlineSvg from "postcss-inline-svg";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig(({ mode }) => {
-  const config: ViteConfig = {
+  const config: ViteUserConfig = {
     build: {
       lib: {
         entry: ["src/index.ts", "src/component.ts", "src/mt-rich-text-editor.ts"],
