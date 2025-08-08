39d39b8fd0fdffa71b64eb97418045d1205503f5
Taku Amano <taku@taaas.jp>
2025-08-08 09:00:01 +0900

    test: tweaks test data

--- packages/editor/src/editor.test.ts
+++ packages/editor/src/editor.test.ts
@@ -51,9 +51,7 @@ describe("HTML parsing", () => {
     expect(normalizeHTML(output)).toBe(normalizeHTML(input));
   });
 
-  it.todo.each([
-    "<p><span class='v1'><span class='v2'><img alt='v' class='v' height='v' src='v' style='top:0' title='v' width='v' /></span><br /></span></p>",
-  ])("TODO:should preserve HTML structure through Tiptap: %s", (input) => {
+  it.todo.each([])("TODO:should preserve HTML structure through Tiptap: %s", (input) => {
     editor.setContent(input);
     const output = editor.getContent();
     expect(normalizeHTML(output)).toBe(normalizeHTML(input));
@@ -64,15 +62,17 @@ describe("HTML parsing", () => {
       "<p style='top:0'><strong><a href='v' target='v'>t</a><strong><a href='v' target='v'>t</a></strong></strong></p>",
       "<p style='top:0'><strong><a href='v' target='v'>tt</a></strong></p>",
     ],
-    // TBD
     [
       "<p><a href='v' target='v'><span><img alt='v' class='v' height='v' src='v' style='top:0' title='v' width='v' /></span></a></p>",
       "<p><span><a href='v' target='v'><img alt='v' class='v' height='v' src='v' style='top:0' title='v' width='v' /></a></span></p>",
     ],
-    // TBD
     [
       "<p><span style='top:0'><strong>test</strong></span></p>",
       "<p><strong><span style='top:0'>test</span></strong></p>",
+    ],
+    [
+      "<p><span class='v1'>a<span class='v2'><img alt='v' class='v' height='v' src='v' style='top:0' title='v' width='v' /></span>b</span></p>",
+      "<p><span class='v1'>a</span><span class='v2'><img alt='v' class='v' height='v' src='v' style='top:0' title='v' width='v' /></span><span class='v1'>b</span></p>",
     ],
   ])("should convert HTML structure through Tiptap: %s", (input, expected) => {
     editor.setContent(input);
