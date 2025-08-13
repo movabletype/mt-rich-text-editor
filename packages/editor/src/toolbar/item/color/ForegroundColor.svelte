df66546cb5e79f836de9766ea8ddf4f1d31e2196
Taku Amano <taku@taaas.jp>
2025-08-14 06:12:58 +0900

    Define default colors as constants

--- packages/editor/src/toolbar/item/color/ForegroundColor.svelte
+++ packages/editor/src/toolbar/item/color/ForegroundColor.svelte
@@ -16,6 +16,7 @@
   import { tooltip } from "../../../tooltip";
   import icon from "../../../ui/icon/foregroundColor.svg?raw";
   import Panel from "./Panel.svelte";
+  import { defaultColors } from "./constant";
   import type { ToolbarItemElement } from "../element";
 
   const element = $host<ToolbarItemElement<Options>>();
@@ -24,44 +25,7 @@
   const { options, tiptap } = element;
   let isOpen = $state(false);
 
-  const colors = options.presetColors ?? [
-    "#000000",
-    "#002B76",
-    "#1B4F2A",
-    "#783F04",
-    "#700000",
-    "#351C75",
-    "#6B6B6B",
-    "#1155CC",
-    "#38761D",
-    "#B45F06",
-    "#AC0000",
-    "#674EA7",
-    "#999999",
-    "#4A86E8",
-    "#6AA84F",
-    "#E69138",
-    "#DC3D3D",
-    "#8E7CC3",
-    "#D9D9D9",
-    "#A4C2F4",
-    "#B6D7A8",
-    "#F9CB9C",
-    "#EA9999",
-    "#B4A7D6",
-    "#F3F3F3",
-    "#CFE2F3",
-    "#D9EAD3",
-    "#FFE6C4",
-    "#F4CCCC",
-    "#D9D2E9",
-    "#FFFFFF",
-    "#EAF1FB",
-    "#EBF5EB",
-    "#FFF8E1",
-    "#FDEBEE",
-    "#F2EFF9",
-  ];
+  const colors = options.presetColors ?? defaultColors;
 
   let selectedColor = $state("#000000");
   element.onEditorUpdate = () => {
