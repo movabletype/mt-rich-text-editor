b8067340f55ce6423c836564f84607f0e7e387e8
Taku Amano <taku@taaas.jp>
2025-08-17 16:31:34 +0900

    td and th to accept block elements

--- packages/editor/src/tiptap/extension/table-header.ts
+++ packages/editor/src/tiptap/extension/table-header.ts
@@ -2,8 +2,6 @@ import { mergeAttributes } from "@tiptap/core";
 import { TableHeader as TiptapTableHeader } from "@tiptap/extension-table-header";
 
 export const TableHeader = TiptapTableHeader.extend({
-  content: "inline*",
-
   addAttributes() {
     return {
       ...this.parent?.(),
