285b88612625356a12ff272656c35726ec9a3e88
Taku Amano <taku@taaas.jp>
2025-08-30 11:27:53 +0900

    Update playwright test for new editor

--- packages/editor/playwright-tests/editor_manager.spec.ts
+++ packages/editor/playwright-tests/editor_manager.spec.ts
@@ -1,37 +1,23 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
+import type {} from "../src/mt-rich-text-editor";
 
-test.describe('EditorManager', () => {
-  test('initialize', async ({ page }) => {
-    await page.goto('/');
-    const editorExists = await page.locator('[data-id="editor"] .ql-editor').isVisible();
+test.describe("EditorManager", () => {
+  test("initialize", async ({ page }) => {
+    await page.goto("/");
+    const editorExists = await page.locator('[data-mt-rich-text-editor-id="editor"]').isVisible();
     expect(editorExists).toBeTruthy();
   });
 
-  test('edit', async ({ page }) => {
-    await page.goto('/');
-    const textarea = await page.locator('#editor');
-    await page.locator('[data-id="editor"] .ql-editor').fill('Test text');
+  test("edit", async ({ page }) => {
+    await page.goto("/");
+    const textarea = await page.locator("#editor");
+    await page.locator('[data-mt-rich-text-editor-id="editor"]').click();
+    await page.keyboard.type("Test text");
     await page.evaluate(async () => {
       await window.MTRichTextEditor.save();
     });
 
     const content = await textarea.evaluate((el: HTMLTextAreaElement) => el.value);
-    expect(content).toBe('<p>Test&nbsp;text</p>');
+    expect(content).toBe("<p>Test text</p>");
   });
-
-  // test('get and manipulate editor', async ({ page }) => {
-  //   await page.setContent(`<div id="editor1"></div>`);
-
-  //   await page.evaluate(() => {
-  //     const manager = new EditorManager();
-  //     manager.initializeEditor('#editor1');
-  //   });
-
-  //   // Input text to editor
-  //   await page.locator('#editor1 .ql-editor').fill('Test text');
-
-  //   // Verify the text was correctly input
-  //   const content = await page.locator('#editor1 .ql-editor').textContent();
-  //   expect(content).toBe('Test text');
-  // });
-}); 
+});
