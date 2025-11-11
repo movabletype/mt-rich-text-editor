af1823ff43c85b2c5a785bb87a1794939c7e25d0
Taku Amano <taku@taaas.jp>
2025-11-11 22:11:11 +0900

    Define default block and inline elements

--- packages/editor/src/constant.ts
+++ packages/editor/src/constant.ts
@@ -1,1 +1,43 @@
 export const WINDOW_EDGE_MARGIN = 10;
+
+export const DEFAULT_BLOCK_ELEMENTS = [
+  "article",
+  "aside",
+  "details",
+  "dialog",
+  "figure",
+  "figcaption",
+  "footer",
+  "header",
+  "hgroup",
+  "main",
+  "nav",
+  "section",
+
+  // Not added because it is rarely used, but may be a candidate to add in the future
+  // "abbr",
+  // "address",
+  // "bdi",
+  // "bdo",
+  // "cite",
+  // "data",
+  // "kbd",
+  // "map", // and "area"
+  // "mark",
+  // "meter",
+  // "q",
+  // "samp",
+  // "search",
+  // "slot",
+];
+
+export const DEFAULT_INLINE_ELEMENTS = [
+  "span",
+  "ruby",
+  "rt",
+  "rp",
+
+  // Not added because it is rarely used, but may be a candidate to add in the future
+  // "time",
+  // "var",
+];
