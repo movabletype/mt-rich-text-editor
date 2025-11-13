1938d14769a5c3b6da9361dddf52bf19c7f2959f
Taku Amano <taku@taaas.jp>
2025-11-13 09:27:31 +0900

    Enable to emit event via TipTap editor commands.

--- packages/editor/src/tiptap/extension/movable-type.ts
+++ packages/editor/src/tiptap/extension/movable-type.ts
@@ -2,10 +2,20 @@ import { Extension } from "@tiptap/core";
 
 import { Global } from "./movable-type/global";
 import { MTTag } from "./movable-type/mt-tag";
+import type { Editor, Events } from "../../editor";
 
 export interface MovableTypeOptions {
+  editor: Editor;
   additionalGlobalAttributeTypes?: string[];
   tags: string[];
+}
+
+declare module "@tiptap/core" {
+  interface Commands {
+    movableType: {
+      emitEditorEvent: <K extends keyof Events>(name: K, data: Events[K]) => void;
+    };
+  }
 }
 
 export const MovableType = Extension.create<MovableTypeOptions>({
@@ -20,5 +30,15 @@ export const MovableType = Extension.create<MovableTypeOptions>({
         tags: this.options.tags,
       }),
     ];
+  },
+
+  addCommands() {
+    return {
+      emitEditorEvent:
+        <K extends keyof Events>(name: K, data: Events[K]) =>
+        () => {
+          this.options.editor.emit(name, data);
+        },
+    };
   },
 });
